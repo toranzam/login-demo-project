8cac3bdde6617e4f5df544324adf719c2992a44f
toranzam <nbc05829@gmail.com>
2023-10-22 05:06:29 +0900

    first commit

---
+++ src/main/java/com/kim/logindemoproject/LoginDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kim.logindemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kim/logindemoproject/LoginDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.logindemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
